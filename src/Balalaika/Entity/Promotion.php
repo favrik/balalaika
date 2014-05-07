f7be4c000db054947ef49254a1bf628bc21249c8
Favio Manriquez Leon <favio@favrik.com>
2014-05-07 16:32:12 -0700

    Add promotion entity

---
+++ src/Balalaika/Entity/Promotion.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Balalaika\Entity;
+
+class Promotion
+{
+    protected $name;
+
+    protected $activators = array();
+    protected $rules = array();
+    protected $actions = array();
+
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }
+
+    public function apply($subject)
+    {
+        if ($this->isEligible($subject)) {
+            foreach ($this->actions as $action) {
+                $action->perform($subject);
+            }
+        }
+    }
+
+    public function isEligible(PromotionSubjectInterface $subject)
+    {
+        return $this->isActive($subject) && $this->isValid($subject);
+    }
+
+    protected function isActive($subject)
+    {
+        foreach ($this->activators as $activator) {
+            if (!$activator->isActive($subject)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    protected function isValid($subject)
+    {
+        foreach ($this->rules as $rule) {
+            if (!$rule->isValid($subject)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public function setActivators(array $activators)
+    {
+        $this->activators = $activators;
+        return $this;
+    }
+
+    public function setRules(array $rules)
+    {
+        $this->rules = $rules;
+        return $this;
+    }
+
+    public function setActions(array $actions)
+    {
+        $this->actions = $actions;
+        return $this;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+}
---
+++ tests/Balalaika/Entity/PromotionTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Balalaika\Entity;
+
+use Balalaika\Entity\Rule\OrderTotalRule;
+use Balalaika\Entity\Activator\CodeActivator;
+use Balalaika\Action\DiscountFixedAmountAction;
+use Balalaika\Action\FreeShippingAction;
+use Balalaika\TestPromotionSubject;
+
+class PromotionTest extends \PHPUnit_Framework_TestCase
+{
+    public function testConstructor()
+    {
+        $promotion = new Promotion('Name');
+        $this->assertEquals('Name', $promotion->getName());
+    }
+
+    public function testSettingRules()
+    {
+        $promotion = new Promotion('Name');
+        $promotion->setRules(array(new OrderTotalRule('>', 80)));
+        $this->assertTrue($promotion->isEligible(new TestPromotionSubject()));
+    }
+
+    public function testSettingActivators()
+    {
+        $promotion = new Promotion('Lala');
+        $codeActivator = new CodeActivator();
+        $codeActivator->code('XYZ');
+        $promotion->setActivators(array($codeActivator));
+        $this->assertFalse($promotion->isEligible(new TestPromotionSubject()));
+    }
+
+    public function testPromotionApply()
+    {
+        $actions = array(
+            new DiscountFixedAmountAction(20),
+            new FreeShippingAction()
+        );
+
+        $subjectMock = $this->getMock('\Balalaika\TestPromotionSubject', array('addOrderDiscount', 'setFreeShipping'));
+        $subjectMock->expects($this->once())->method('addOrderDiscount');
+        $subjectMock->expects($this->once())->method('setFreeShipping');
+
+        $promotion = new Promotion('Lala');
+        $promotion->setActions($actions);
+
+        $this->assertTrue($promotion->isEligible($subjectMock));
+
+        $promotion->apply($subjectMock);
+    }
+}
