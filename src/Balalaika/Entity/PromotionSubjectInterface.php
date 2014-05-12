ee2c0c3020b9243f65d9e2f834b7a7b6cfe8eb0b
Favio Manriquez Leon <favio@favrik.com>
2014-05-12 14:34:47 -0700

    Add code comments to promotion subject interface

--- src/Balalaika/Entity/PromotionSubjectInterface.php
+++ src/Balalaika/Entity/PromotionSubjectInterface.php
@@ -1,6 +1,5 @@
 <?php
 
-
 namespace Balalaika\Entity;
 
 /**
@@ -16,10 +15,24 @@ interface PromotionSubjectInterface
      */
     public function getOrderTotal();
 
+    /**
+     * Get product line item total.
+     *
+     * @return int
+     */
     public function getProductTotal();
 
+    /**
+     * Add a discount to the order total.
+     *
+     * @param int $discount
+     */
     public function addOrderDiscount($discount);
 
+    /**
+     * Sets the order shipping to 0 added as a discount.  TODO: this could be more abstract
+     * and only use one method: "AddPromotion()"
+     */
     public function setFreeShipping();
 
     /**
@@ -30,24 +43,49 @@ interface PromotionSubjectInterface
     /**
      * Get the shipping address zipcode.
      *
-     * @return mixed
+     * @return int
      */
     public function getShippingZipcode();
 
     /**
      * Get the user entity identifier.
      *
-     * @return mixed
+     * @return int
      */
     public function getUserId();
 
+    /**
+     * Get a DateTime value according to the system settings.
+     *
+     * @param mixed $dateTime - Commonly a string (2014-04-16 06:00) or an int for timestamps.
+     */
     public function getDateTime($dateTime = null);
 
+    /**
+     * Get a comparable DateTime value using the system settings.
+     *
+     * @return mixed
+     */
     public function getCurrentDateTime();
 
+    /**
+     * Get the customer submitted promotion code. 
+     *
+     * @return string
+     */
     public function getPromotionCode();
 
+    /**
+     * Get how many times this promotion has been used by the system.
+     *
+     * @return int
+     */
     public function getPromotionUsageCount();
 
+    /**
+     * Get how many times this promotion has been used by the order owner.
+     *
+     * @return int
+     */
     public function getPromotionUserCount();
 }
