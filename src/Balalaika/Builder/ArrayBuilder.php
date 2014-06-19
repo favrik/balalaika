3d2053c71ff22fef5060eb33467fb2903ae4f874
Favio Manriquez Leon <favio@favrik.com>
2014-06-18 19:00:58 -0700

    Take component name from array value, not index

--- src/Balalaika/Builder/ArrayBuilder.php
+++ src/Balalaika/Builder/ArrayBuilder.php
@@ -86,37 +86,39 @@ class ArrayBuilder
     }
 
     public function build($data) {
-	    $this->data = $data;
+        $this->data = $data;
 
-	    $promotion = new Promotion($data['name']);
+        $promotion = new Promotion($data['name']);
 
-	    if ($data['has_code']) {
-	        $this->activators['code']
-	             ->code($data['submitted_code'])
-	             ->usageLimitPerCode($data['usage_limit_per_code'])
-	             ->usageLimitPerUser($data['usage_limit_per_user']);
-	    }
-	    $this->activators['date_range']->start($data['start'])->end($data['end']);
-	    $promotion->setActivators($this->activators);
+        if ($data['has_code']) {
+            $this->activators['code']
+              ->code($data['submitted_code'])
+              ->usageLimitPerCode($data['usage_limit_per_code'])
+              ->usageLimitPerUser($data['usage_limit_per_user']);
+        }
+        $this->activators['date_range']->start($data['start'])->end($data['end']);
+        $promotion->setActivators($this->activators);
 
-	    $rules = array();
-	    foreach ($data['rules'] as $name => $rule) {
-	        $current = $this->rules[$name]['object'];
-	        $current->initialize($rule['operator'], $rule['value']);
-	        $rules[] = $current;
-	    }
-	    $promotion->setRules($rules);
+        $rules = array();
+        foreach ($data['rules'] as $index => $rule) {
+            $name = $rule['component_name'];
+            $current = $this->rules[$name]['object'];
+            $current->initialize($rule['operator'], $rule['value']);
+            $rules[] = $current;
+        }
+        $promotion->setRules($rules);
 
-	    $actions = array();
-	    foreach ($data['actions'] as $name => $action) {
-	        $current = $this->actions[$name]['object'];
-	        // Quite hacky, because all actions are stored in the same table
-	        // the argument column has type ambiguity.
-	        $current->initialize(intval($action['argument']));
-	        $actions[] = $current;
-	    }
-	    $promotion->setActions($actions);
+        $actions = array();
+        foreach ($data['actions'] as $index => $action) {
+            $name = $action['component_name'];
+            $current = $this->actions[$name]['object'];
+            // Quite hacky, because all actions are stored in the same table
+            // the argument column has type ambiguity.
+            $current->initialize(intval($action['argument']));
+            $actions[] = $current;
+        }
+        $promotion->setActions($actions);
 
-	    return $promotion;
+        return $promotion;
     }
 }
